b8de615afb4d3e8f255043594f44d6584c7a6a90
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2025-12-01 19:04:17 -0300

    Create testefunc1.go
    testando função básica 1

---
+++ testefunc1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func imprimirdados(nome string, idade int) {
+
+	fmt.Printf("Meu nome é %s e minha idade é %d", nome, idade)
+}
+
+func main() {
+	imprimirdados("kassiany", 21)
+}
