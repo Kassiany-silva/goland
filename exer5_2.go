a79a3a7353203d27e2f10af5fd4e1a61a0eb3291
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2025-12-29 21:16:02 -0300

    Create exer5_2.go

---
+++ exer5_2.go
@@ -0,0 +1,17 @@
+/*
+Crie uma variável de tipo string utilizando uma raw string literal.
+Demonstre-a.
+*/
+package main
+
+import "fmt"
+
+func main() {
+
+	x := `isso 
+			É um 
+				teste 
+				HI GO`
+	fmt.Println(x)
+
+}
