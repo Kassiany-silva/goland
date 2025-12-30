1ce3411d9f8a9a30074c8339622d1187fb6ce9c9
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2025-12-30 15:50:53 -0300

    Create desafiosurpresa.go

---
+++ desafiosurpresa.go
@@ -0,0 +1,11 @@
+// Faça um loop dos números 33 a 122, e utilize format printing para demonstrá-los como texto/string.
+package main
+
+import "fmt"
+
+func main() {
+	for x := 33; x <= 122; x++ {
+		fmt.Printf("%c\n", x)
+	}
+
+}
