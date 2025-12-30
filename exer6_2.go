f79d6990a4cd08e3e9d4a865b5593789f7d50109
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2025-12-29 21:20:15 -0300

    Create exer6_2.go

---
+++ exer6_2.go
@@ -0,0 +1,21 @@
+/*
+Utilizando iota, crie 4 constantes cujos valores sejam os próximos 4 anos.
+Demonstre estes valores.
+*/
+package main
+
+import "fmt"
+
+const (
+	_ = 2004 + iota
+	b
+	c
+	d
+	e
+)
+
+func main() {
+
+	fmt.Println(b, c, d, e)
+
+}
