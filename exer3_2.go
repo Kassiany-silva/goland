a9058e8e4a39f322d69ffcf248fe71ed91d85c32
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2025-12-29 21:11:39 -0300

    Create exer3_2.go

---
+++ exer3_2.go
@@ -0,0 +1,15 @@
+/*
+Crie constantes tipadas e não-tipadas.
+Demonstre seus valores.
+*/
+package main
+
+import "fmt"
+
+const a int = 10
+const b = 10
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+}
