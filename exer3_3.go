c680f46c729d09d41bca8570d5a95bede25c0376
Kassiany Alves <119141822+Kassiany-silva@users.noreply.github.com>
2026-01-02 10:29:01 -0300

    Create exer3_3.go

---
+++ exer3_3.go
@@ -0,0 +1,14 @@
+// Crie um loop utilizando a sintaxe: for condition {}
+// Utilize-o para demonstrar os anos desde que você nasceu.
+package main
+
+import "fmt"
+
+func main() {
+	meu := 2004
+	contador := 2050
+	for meu <= contador {
+		fmt.Println(meu)
+		meu++
+	}
+}
